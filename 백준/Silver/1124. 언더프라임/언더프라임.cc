d1a38e085f76eb7487605f63c30e612004e8c47b
Park Seunghyeon <aoiaobaprod@gmail.com>
2026-02-25 10:49:49 +0900

    [Silver I] Title: 언더프라임, Time: 4 ms, Memory: 2508 KB -BaekjoonHub

--- 백준/Silver/1124. 언더프라임/언더프라임.cc
+++ 백준/Silver/1124. 언더프라임/언더프라임.cc
@@ -4,44 +4,50 @@
 #define fastio() ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 using namespace std;
 
-int a, b, sieve[101010], ans = 0;
+const int MAX = 100000;
+bool isPrime[MAX + 1];
+int factorCnt[MAX + 1];
 
 // 에라토스테네스의 체를 활용하여 밑작업
 void find_prime() {
-    fill(sieve, sieve + 101010, 1);
-    sieve[0] = sieve[1] = 0;
+    fill(isPrime, isPrime + MAX + 1, true);
+    isPrime[0] = isPrime[1] = false;
 
-    for (int i = 2; i * i < 101010; i++) {
-        if (sieve[i]) {
-            for (int j = i * i; j < 101010; j += i) {
-                sieve[j] = 0;
+    for (int i = 2; i * i <= MAX; i++) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= MAX; j += i)
+                isPrime[j] = false;
+        }
+    }
+}
+
+// 소인수분해 전처리 함수
+void prime_factorization() {
+    for (int i = 2; i <= MAX; i++) {
+        if (isPrime[i]) {
+            for (int j = i; j <= MAX; j += i) {
+                int temp = j;
+                while (temp % i == 0) {
+                    factorCnt[j]++;
+                    temp /= i;
+                }
             }
         }
     }
 }
 
-// 소인수분해 함수
-int prime_factorization(int n) {
-    int cnt = 0;
-    if (sieve[n]) return 1; // 소수면
-    
-    int i = 2;
-    while (n > 1) {
-        if (n % i == 0) {
-            n /= i;
-            cnt++;
-        } else i++;
-    }
-    return cnt;
-}
 
 int main(void) {
     fastio();
     find_prime();
+    prime_factorization();
 
+    int a, b;
     cin >> a >> b;
+
+    int ans = 0;
     for (int i = a; i <= b; i++) {
-        if (sieve[prime_factorization(i)])
+        if (isPrime[factorCnt[i]])
             ans++;
     }
 
