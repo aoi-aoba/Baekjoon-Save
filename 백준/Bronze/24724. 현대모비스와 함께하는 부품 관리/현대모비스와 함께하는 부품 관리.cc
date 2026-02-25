4824c056afa4982dcc829c8b7f8b75e307e40468
Park Seunghyeon <aoiaobaprod@gmail.com>
2026-02-25 09:08:55 +0900

    [Bronze IV] Title: 현대모비스와 함께하는 부품 관리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.cc
@@ -0,0 +1,16 @@
+// 현대모비스와 함께하는 부품 관리
+
+#include <bits/stdc++.h>
+#define fastio() ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+
+int main(void) {
+    fastio();
+    int t;
+    cin >> t;
+    for (int i = 1; i <= t; i++) {
+        cout << "Material Management " << i << "\n";
+        cout << "Classification ---- End!\n";
+    }
+    return 0;
+}
